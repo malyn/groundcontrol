99ab3b55cc18f8e33633e71ae56c10cc49d5b48b
Michael Alyn Miller <malyn@strangeGizmo.com>
2022-10-25 16:12:50 -0700

    Validate proper command execution in startup tests

--- tests/startup_tests.rs
+++ tests/startup_tests.rs
@@ -3,12 +3,20 @@
 //! processes into their started state).
 
 use groundcontrol::config::{CommandConfig, Config, ProcessConfig};
+use tempfile::TempDir;
 use tokio::sync::mpsc;
 
 /// Verifies that a failed `pre` execution aborts all subsequent command
 /// executions.
 #[tokio::test]
 async fn failed_pre_aborts_startup() {
+    // Create a temp directory into which we can write output from the
+    // commands, as a simple way of verifying that the commands are in
+    // fact run in the proper order.
+    let dir = TempDir::new().unwrap();
+    let result_path = dir.path().join("results.txt").to_str().unwrap().to_string();
+
+    // Create the test configuration.
     let config = Config {
         processes: vec![
             ProcessConfig {
@@ -16,8 +24,8 @@ async fn failed_pre_aborts_startup() {
                 pre: Some(CommandConfig {
                     user: None,
                     env_vars: Default::default(),
-                    program: String::from("/bin/echo"),
-                    args: vec![String::from("a-pre")],
+                    program: String::from("/bin/sh"),
+                    args: vec![String::from("-c"), format!("echo a-pre >> {result_path}")],
                 }),
                 run: None,
                 stop: Default::default(),
@@ -40,8 +48,8 @@ async fn failed_pre_aborts_startup() {
                 pre: Some(CommandConfig {
                     user: None,
                     env_vars: Default::default(),
-                    program: String::from("/bin/echo"),
-                    args: vec![String::from("c-pre")],
+                    program: String::from("/bin/sh"),
+                    args: vec![String::from("-c"), format!("echo c-pre >> {result_path}")],
                 }),
                 run: None,
                 stop: Default::default(),
@@ -54,4 +62,7 @@ async fn failed_pre_aborts_startup() {
     let (_tx, rx) = mpsc::unbounded_channel();
     let result = groundcontrol::run(config, rx).await;
     assert!(result.is_err());
+
+    let result = tokio::fs::read_to_string(result_path).await.unwrap();
+    assert_eq!("a-pre\n", result);
 }
