2affe2cbf771270c7c1c0ed4001d6cdb1fecd52a
Michael Alyn Miller <malyn@strangeGizmo.com>
2022-10-25 09:27:35 -0700

    Move `run` logic from `main.rs` to `lib.rs`
    Will make it easier to unit test the actual Ground Control code without
    having to run the `groundcontrol` binary.

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,74 @@
     clippy::unwrap_used
 )]
 
+use anyhow::Context;
+use config::Config;
+use tokio::sync::mpsc;
+
+use crate::process::Process;
+
 pub mod command;
 pub mod config;
 pub mod process;
+
+/// Runs a Ground Control specification, returning only when all of the
+/// processes have stopped (either because one process triggered a
+/// shutdown, or because the provide shutdown signal was triggered).
+pub async fn run(config: Config, mut shutdown: mpsc::UnboundedReceiver<()>) -> anyhow::Result<()> {
+    // Create the shutdown channel, which will be used to initiate the
+    // shutdown process, regardless of if this is a graceful shutdown
+    // triggered by a shutdown signal, or an unexpected shutdown caused
+    // by the failure of a daemon process.
+    let (shutdown_sender, mut shutdown_receiver) = mpsc::unbounded_channel();
+
+    // Start every process in the order they were found in the config
+    // file.
+    let mut processes: Vec<Process> = Default::default();
+    for process_config in config.processes.into_iter() {
+        // TODO: Need to start shutting down if this fails.
+        let process = Process::start(process_config, shutdown_sender.clone())
+            .await
+            .with_context(|| "Unable to start process")?;
+        processes.push(process);
+    }
+
+    // Convert an external shutdown signal into a shutdown message.
+    let external_shutdown_sender = shutdown_sender.clone();
+    tokio::spawn(async move {
+        // Both sending the shutdown signal, *and dropping the sender,*
+        // trigger a shutdown.
+        let _ = shutdown.recv().await;
+        let _ = external_shutdown_sender.send(());
+    });
+
+    tracing::info!(
+        process_count = %processes.len(),
+        "Startup phase completed; waiting for shutdown signal or any process to exit."
+    );
+
+    shutdown_receiver
+        .recv()
+        .await
+        .expect("All shutdown senders closed without sending a shutdown signal.");
+
+    // Either one process exited or we received a stop signal; stop all
+    // of the processes in the *reverse* order in which they were
+    // started.
+    tracing::info!("Completion signal triggered; shutting down all processes");
+
+    while let Some(process) = processes.pop() {
+        // TODO: We could do some sort of thing here where we check to
+        // see if this is the process that triggered the shutdown and,
+        // *still* `stop` it (since we may need to run `post`), but not
+        // actually kill it, since it has already stopped. Basically,
+        // just some extra tracking to avoid the WARN log that happens
+        // when trying to kill a process that has already exited.
+        if let Err(err) = process.stop().await {
+            tracing::error!(?err, "Error stopping process");
+        }
+    }
+
+    tracing::info!("All processes have exited.");
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,11 @@
 
 use anyhow::Context;
 use clap::Parser;
-use groundcontrol::{config::Config, process::Process};
+use groundcontrol::config::Config;
 use tokio::{
     signal::unix::{signal, SignalKind},
     sync::mpsc,
 };
-use tracing::Level;
 
 #[derive(Parser)]
 #[clap(about, long_about = None)]
@@ -40,7 +39,7 @@ async fn main() -> anyhow::Result<()> {
     // Tokio task, since we want panic to mean "something is very wrong;
     // stop everything").
     std::panic::set_hook(Box::new(|info| {
-        eprintln!("Server panicked: {info}");
+        eprintln!("Process panicked: {info}");
         std::process::abort();
     }));
 
@@ -56,6 +55,7 @@ async fn main() -> anyhow::Result<()> {
     // `tracing_subscriber::fmt().init()` (the latter does *not*
     // automatically bring in EnvFilter, for example):
     // <https://github.com/tokio-rs/tracing/issues/1329#issuecomment-808682793>
+    // TODO: We don't actually need this; this was only required back when we supported text *or* JSON.
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .with_writer(std::io::stdout)
@@ -75,24 +75,10 @@ async fn main() -> anyhow::Result<()> {
         return Ok(());
     }
 
-    // Create the shutdown channel, which will be used to initiate the
-    // shutdown process, regardless of if this is a graceful shutdown
-    // triggered by a UNIX signal, or an unexpected shutdown caused by
-    // the failure of a daemon process.
-    let (shutdown_sender, mut shutdown_receiver) = mpsc::unbounded_channel();
+    // Create the external shutdown signal (used to shut down Ground
+    // Control on UNIX signals).
+    let (shutdown_sender, shutdown_receiver) = mpsc::unbounded_channel();
 
-    // Start every process in the order they were found in the config
-    // file.
-    let mut processes: Vec<Process> = Default::default();
-    for process_config in config.processes.into_iter() {
-        // TODO: Need to start shutting down if this fails.
-        let process = Process::start(process_config, shutdown_sender.clone())
-            .await
-            .with_context(|| "Unable to start process")?;
-        processes.push(process);
-    }
-
-    // Convert signals into shutdown messages.
     let sigint_shutdown_sender = shutdown_sender.clone();
     tokio::spawn(async move {
         signal(SignalKind::interrupt())
@@ -111,34 +97,10 @@ async fn main() -> anyhow::Result<()> {
         let _ = sigterm_shutdown_sender.send(());
     });
 
-    tracing::info!(
-        process_count = %processes.len(),
-        "Startup phase completed; waiting for shutdown signal or any process to exit."
-    );
-
-    shutdown_receiver
-        .recv()
+    // Run the Ground Control specification.
+    groundcontrol::run(config, shutdown_receiver)
         .await
-        .expect("All shutdown senders closed without sending a shutdown signal.");
-
-    // Either one process exited or we received a stop signal; stop all
-    // of the processes in the *reverse* order in which they were
-    // started.
-    tracing::info!("Completion signal triggered; shutting down all processes");
-
-    while let Some(process) = processes.pop() {
-        // TODO: We could do some sort of thing here where we check to
-        // see if this is the process that triggered the shutdown and,
-        // *still* `stop` it (since we may need to run `post`), but not
-        // actually kill it, since it has already stopped. Basically,
-        // just some extra tracking to avoid the WARN log that happens
-        // when trying to kill a process that has already exited.
-        if let Err(err) = process.stop().await {
-            tracing::error!(?err, "Error stopping process");
-        }
-    }
-
-    tracing::event!(Level::INFO, "All processes have exited.");
+        .with_context(|| "Ground Control did not shut down cleanly")?;
 
     Ok(())
 }
