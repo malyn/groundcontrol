9f78ec13faa7495ff096c3ebf551df01ef602e82
Michael Alyn Miller <malyn@strangeGizmo.com>
2022-10-25 16:26:29 -0700

    Use (TOML) config for the integration tests
    Verifies that we are testing the config file format, but is also just
    much shorter/clearer than creating the `Config` structs.

--- tests/startup_tests.rs
+++ tests/startup_tests.rs
@@ -2,13 +2,14 @@
 //! "startup" is defined as the process of getting all long-running
 //! processes into their started state).
 
-use groundcontrol::config::{CommandConfig, Config, ProcessConfig};
+use groundcontrol::config::Config;
 use tempfile::TempDir;
 use tokio::sync::mpsc;
 
 /// Verifies that a failed `pre` execution aborts all subsequent command
-/// executions.
-#[tokio::test]
+/// executions *and* runs stop/post commands for anything that was
+/// started.
+#[test_log::test(tokio::test)]
 async fn failed_pre_aborts_startup() {
     // Create a temp directory into which we can write output from the
     // commands, as a simple way of verifying that the commands are in
@@ -16,53 +17,33 @@ async fn failed_pre_aborts_startup() {
     let dir = TempDir::new().unwrap();
     let result_path = dir.path().join("results.txt").to_str().unwrap().to_string();
 
-    // Create the test configuration.
-    let config = Config {
-        processes: vec![
-            ProcessConfig {
-                name: String::from("a"),
-                pre: Some(CommandConfig {
-                    user: None,
-                    env_vars: Default::default(),
-                    program: String::from("/bin/sh"),
-                    args: vec![String::from("-c"), format!("echo a-pre >> {result_path}")],
-                }),
-                run: None,
-                stop: Default::default(),
-                post: None,
-            },
-            ProcessConfig {
-                name: String::from("b"),
-                pre: Some(CommandConfig {
-                    user: None,
-                    env_vars: Default::default(),
-                    program: String::from("/user/binary/nope"),
-                    args: vec![],
-                }),
-                run: None,
-                stop: Default::default(),
-                post: None,
-            },
-            ProcessConfig {
-                name: String::from("c"),
-                pre: Some(CommandConfig {
-                    user: None,
-                    env_vars: Default::default(),
-                    program: String::from("/bin/sh"),
-                    args: vec![String::from("-c"), format!("echo c-pre >> {result_path}")],
-                }),
-                run: None,
-                stop: Default::default(),
-                post: None,
-            },
-        ],
-    };
+    // Create and parse the test configuration.
+    let config: Config = toml::from_str(&format!(
+        r##"
+        [[processes]]
+        name = "a"
+        pre = [ "/bin/sh", "-c", "echo a-pre >> {result_path}" ]
+        post = [ "/bin/sh", "-c", "echo a-post >> {result_path}" ]
 
-    // Run the specification; only `a-pre` should run.
+        [[processes]]
+        name = "b"
+        pre = "/user/binary/nope"
+        run = [ "/bin/sh", "-c", "echo b-run >> {result_path}" ]
+        post = [ "/bin/sh", "-c", "echo b-post >> {result_path}" ]
+
+        [[processes]]
+        name = "c"
+        pre = [ "/bin/sh", "-c", "echo c-pre >> {result_path}" ]
+        post = [ "/bin/sh", "-c", "echo c-post >> {result_path}" ]
+        "##
+    ))
+    .unwrap();
+
+    // Run the specification; only `a-pre` and `a-post` should run.
     let (_tx, rx) = mpsc::unbounded_channel();
     let result = groundcontrol::run(config, rx).await;
     assert!(result.is_err());
 
     let result = tokio::fs::read_to_string(result_path).await.unwrap();
-    assert_eq!("a-pre\n", result);
+    assert_eq!("a-pre\na-post\n", result);
 }
